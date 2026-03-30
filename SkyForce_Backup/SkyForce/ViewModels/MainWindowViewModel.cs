5edbbdba9bc92b3a098fd5e9877fd5b467378434
LucasLacerda1 <lacerdafreire86@gmail.com>
2026-03-30 15:11:07 -0300

    Cleanup: Removendo backups e arquivos de configuração

--- SkyForce_Backup/SkyForce/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SkyForce.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- SkyForce_Backup/SkyForce/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace SkyForce.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
